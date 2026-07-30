cd06f2715452b7666ff5679057c24bb40be89dd5
Rishabh <amanmauryaji56@gmail.com>
2026-07-30 19:20:26 +0530

    22

---
+++ 2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+const results = document.querySelector('#results');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const height_int = parseInt(height);
+    const weight_int = parseInt(weight);
+
+    // console.log(height);
+    // console.log(height_int);
+    if(height<=0 || isNaN(height) || height ===''){
+        results.innerHTML = `"${height}", is not a valid height`
+    }
+    else if(weight<=0 || isNaN(weight) || weight == ''){
+        results.innerHTML = `"${weight}", is not a vaild weight`;
+    }
+    else {
+        const bmi = ((weight_int / (height_int * height_int)) * 10000).toFixed(2);
+        results.innerHTML = `<span> "${bmi}", is your BMI </span>`;
+        const msg = document.createAttribute('span');
+        results.appendChild(msg);
+        if(bmi <= 18.6) msg.innerHTML = `UnderWeight`;
+        else if(bmi > 18.6 && bmi <= 24.9) msg.innerHTML = `NormalWeight`;
+        else msg.innerHTML = `OverWeight`
+    }
+});
