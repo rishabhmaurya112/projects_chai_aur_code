05a3385d9849a620f7af68a91b570c34f8c5083f
Rishabh <amanmauryaji56@gmail.com>
2026-07-30 13:11:31 +0530

    first project done

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (btn) {
+    body.addEventListener('click', function (evnt){
+        console.log(evnt.target);
+        if(evnt.target.id === 'grey'){
+            body.style.backgroundColor = "grey";
+        }
+        else if(evnt.target.id === 'white'){
+            body.style.backgroundColor = "white";
+        }
+        else if(evnt.target.id === 'blue'){
+            body.style.backgroundColor = "blue";
+        }
+        else{
+            body.style.backgroundColor = "yellow";
+        }
+    });
+});
+
+//not true (i tried optimised but it didnt worked)
+// const button = document.querySelector('.button');
+// const body = document.querySelector('body');
+
+// button.addEventListener('click', function (evnt){
+//     // console.log(evnt);
+//     // console.log(evnt.target.id);
+//     body.style.backgroundColor = evnt.target.id;
+// });
