fb8ec808e76241f52475a1803bed440b79bac053
Rishabh <amanmauryaji56@gmail.com>
2026-07-31 17:28:46 +0530

    final touch

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -1,30 +1,44 @@
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach(function (btn) {
+//     body.addEventListener('click', function (evnt){
+//         console.log(evnt.target);
+//         if(evnt.target.id === 'grey'){
+//             body.style.backgroundColor = "grey";
+//         }
+//         else if(evnt.target.id === 'white'){
+//             body.style.backgroundColor = "white";
+//         }
+//         else if(evnt.target.id === 'blue'){
+//             body.style.backgroundColor = "blue";
+//         }
+//         else{
+//             body.style.backgroundColor = "yellow";
+//         }
+//     });
+// });
+
+// //not true (i tried optimised but it didnt worked)
+// // const button = document.querySelector('.button');
+// // const body = document.querySelector('body');
+
+// // button.addEventListener('click', function (evnt){
+// //     // console.log(evnt);
+// //     // console.log(evnt.target.id);
+// //     body.style.backgroundColor = evnt.target.id;
+// // });
+
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-buttons.forEach(function (btn) {
-    body.addEventListener('click', function (evnt){
-        console.log(evnt.target);
-        if(evnt.target.id === 'grey'){
-            body.style.backgroundColor = "grey";
-        }
-        else if(evnt.target.id === 'white'){
-            body.style.backgroundColor = "white";
-        }
-        else if(evnt.target.id === 'blue'){
-            body.style.backgroundColor = "blue";
-        }
-        else{
-            body.style.backgroundColor = "yellow";
-        }
+buttons.forEach(element => {
+    element.addEventListener('click', function(e){
+        const color_id = e.target.id;
+        console.log(color_id);
+        if(color_id == "grey") body.style.backgroundColor = color_id;
+        else if(color_id == "white") body.style.backgroundColor = color_id;
+        else if(color_id == "blue") body.style.backgroundColor = color_id;
+        else if(color_id == "yellow") body.style.backgroundColor = color_id;
     });
 });
-
-//not true (i tried optimised but it didnt worked)
-// const button = document.querySelector('.button');
-// const body = document.querySelector('body');
-
-// button.addEventListener('click', function (evnt){
-//     // console.log(evnt);
-//     // console.log(evnt.target.id);
-//     body.style.backgroundColor = evnt.target.id;
-// });
--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,28 +1,68 @@
+// const form = document.querySelector('form');
+// const results = document.querySelector('#results');
+
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const height = document.querySelector('#height').value;
+//     const weight = document.querySelector('#weight').value;
+//     const height_int = parseInt(height);
+//     const weight_int = parseInt(weight);
+
+//     // console.log(height);
+//     // console.log(height_int);
+//     if(height<=0 || isNaN(height) || height ===''){
+//         results.innerHTML = `"${height}", is not a valid height`
+//     }
+//     else if(weight<=0 || isNaN(weight) || weight == ''){
+//         results.innerHTML = `"${weight}", is not a vaild weight`;
+//     }
+//     else {
+//         const bmi = ((weight_int / (height_int * height_int)) * 10000).toFixed(2);
+//         results.innerHTML = `<span> "${bmi}", is your BMI </span>`;
+//         const msg = document.createAttribute('span');
+//         results.appendChild(msg);
+//         if(bmi <= 18.6) msg.innerHTML = `UnderWeight`;
+//         else if(bmi > 18.6 && bmi <= 24.9) msg.innerHTML = `NormalWeight`;
+//         else msg.innerHTML = `OverWeight`
+//     }
+// });
+
 const form = document.querySelector('form');
-const results = document.querySelector('#results');
+const result = document.querySelector('#results');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', function (e){
     e.preventDefault();
-    const height = document.querySelector('#height').value;
-    const weight = document.querySelector('#weight').value;
-    const height_int = parseInt(height);
-    const weight_int = parseInt(weight);
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
 
-    // console.log(height);
-    // console.log(height_int);
-    if(height<=0 || isNaN(height) || height ===''){
-        results.innerHTML = `"${height}", is not a valid height`
+    if(isNaN(height) || height < 0 || height === ''){
+        result.innerHTML = `"${height}", is not a valid height`;
     }
-    else if(weight<=0 || isNaN(weight) || weight == ''){
-        results.innerHTML = `"${weight}", is not a vaild weight`;
+    if(isNaN(weight) || weight < 0 || weight === ''){
+        result.innerHTML = `"${weight}", is not a valid weight`;
     }
-    else {
-        const bmi = ((weight_int / (height_int * height_int)) * 10000).toFixed(2);
-        results.innerHTML = `<span> "${bmi}", is your BMI </span>`;
-        const msg = document.createAttribute('span');
-        results.appendChild(msg);
-        if(bmi <= 18.6) msg.innerHTML = `UnderWeight`;
-        else if(bmi > 18.6 && bmi <= 24.9) msg.innerHTML = `NormalWeight`;
-        else msg.innerHTML = `OverWeight`
+    else{
+        const bmi = ((weight / (height * height)) * 10000).toFixed(2);
+        result.innerHTML = `<span> "${bmi}", is your BMI </span>`;
+
+        const Wguide = document.querySelectorAll('#weight-guide p');
+        Wguide.forEach(element => {
+            console.log(element.id);
+            if(bmi <= 18.6){
+                document.getElementById('UW').style.backgroundColor = 'red';
+                document.getElementById('OW').style.backgroundColor = 'grey';
+                document.getElementById('NW').style.backgroundColor = 'grey';
+            }
+            else if(bmi > 18.6 && bmi <=24.9){
+                document.getElementById('NW').style.backgroundColor = 'green';
+                document.getElementById('UW').style.backgroundColor = 'grey';
+                document.getElementById('OW').style.backgroundColor = 'grey';
+            }
+            else{
+                document.getElementById('OW').style.backgroundColor = 'red';
+                document.getElementById('UW').style.backgroundColor = 'grey';
+                document.getElementById('NW').style.backgroundColor = 'grey';
+            }
+        });
     }
 });
