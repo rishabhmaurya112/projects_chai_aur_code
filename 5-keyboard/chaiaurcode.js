96b6b8dc02aae72a7e8b9589523dc905fc1d7cdc
Rishabh <amanmauryaji56@gmail.com>
2026-08-03 00:54:23 +0530

    project 5

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,21 @@
-console.log('Project 5');
+const details = document.querySelector('.key');
+
+document.addEventListener('keydown', function (e) {
+    console.log(e);
+    details.innerHTML = `
+        <div>
+            <table>
+            <tr>
+                <th>key</th>
+                <th>key Code</th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <td>${e.key === ' ' ? 'space' : e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+        </div>
+    `
+})
